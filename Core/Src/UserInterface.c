f86770e697d139eec0492301bec356aa2ceb2b54
Veli-Matti Kananen <veli-matti.kananen@acuvi.com>
2023-01-09 14:37:28 +0200

    Commit signature test

--- Core/Src/UserInterface.c
+++ Core/Src/UserInterface.c
@@ -74,6 +74,7 @@ __IO uint32_t uwPrescalerValue = 0;
  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 
+
     if (g_pfTimerCallback != NULL) {
         g_pfTimerCallback(g_pTimerPrm);
     }
